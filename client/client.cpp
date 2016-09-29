53b4db1e43162d805bf7aabb515a31f0469ba18d
John Hritz <john-j-hritz@sbcglobal.net>
2016-09-29 12:02:05 -0700

    initial commit

---
+++ client/client.cpp
@@ -0,0 +1,305 @@
+//Name of Authros: John Hritz & Santiago Quinio
+//Course Number and Name: CSE434, Computer Networks
+//Semester: Fall 2016
+//Project Part 1
+//Time Spent: 4 hours 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <ios>
+#include <sstream>
+#include <climits>
+
+//contains definitions of a number of data types used in system calls
+#include <sys/types.h>
+
+//definitions of structures needed for sockets
+#include <sys/socket.h>
+
+//in.h contains constants and structures needed for internet domain addresses
+#include <netinet/in.h>
+//netdb.h defines the structure hostent,
+#include <netdb.h>
+
+
+//variable declarations
+
+//sockfd is a file descriptor
+//portno stores the port number on which the server accepts connections.
+int sockfd, portno, n;
+
+//serv_addr will contain the address of the server
+struct sockaddr_in serv_addr;
+
+//variable server is a pointer to a structure of type hostent
+/*
+struct  hostent
+{
+  char    *h_name;        // official name of host
+  char    **h_aliases;    // alias list
+  int     h_addrtype;     // host address type
+  int     h_length;       // length of address
+  char    **h_addr_list;  // list of addresses from name server
+  #define h_addr  h_addr_list[0]  // address, for backward compatiblity
+};
+*/
+struct hostent *server;
+
+char buffer[256];
+
+void error(const char *msg)
+{
+	perror(msg);
+	exit(0);
+}
+
+/*
+void client_service()
+{
+	int n;
+	char buffer[256];
+	
+	//while connection is open
+	while(1)
+	{
+		n = read(newsockfd,buffer,255);
+
+		if(n<0)
+		{
+			child_error("ERROR reading from socket");
+		}
+		if(n == 0)
+		{
+			child_error("Connection closed by client");
+		}
+		std::cout<<"Message from client " << users.back() << ": " << buffer << std::endl;
+	
+		n = write(newsockfd,"I got your message", 18);
+	}
+}
+ */
+
+void print_server()
+{
+    //print server response
+    std::cout << "Server says: " << std::endl;
+    printf("%s\n", buffer);
+}
+
+void read_server()
+{
+    bzero(buffer, 256);
+    n = read(sockfd, buffer, 255);
+
+    if (n < 0)
+    {
+        error("ERROR reading from socket");
+    }
+
+    if (n == 0)
+    {
+        error("ERROR You have been disconnected by the server");
+    }
+
+    print_server();
+}
+
+int main(int argc, char *argv[])
+{
+    //if too few/many arguements are provided
+    if (argc != 4)
+    {
+        error("ERROR: Incorrect command. Correct command usage is: client <hostname> <client number> <port>.");
+    }
+
+    //if argv[2] is not an integer
+    if (strtol(argv[2], NULL, 0) == 0)
+    {
+        error("ERROR: Invalid client number specified. Client must be an integer. Correct command usage is: client <hostname> <client number> <port>.");
+    }
+
+    //if argv[3] is not an integer
+    if (strtol(argv[3], NULL, 0) == 0)
+    {
+        error("ERROR: Invalid port number specified. Port must be an integer. Correct command usage is: client <hostname> <client number> <port>.");
+    }
+
+    //if argv[3] is <= 1024
+    if (atoi(argv[3]) <= 1024)
+    {
+        error("ERROR: Ports numbered 1024 and below are reserved by the OS.");
+    }
+
+
+
+    //atoi() function can be used to convert port number from a string of digits to an integer, if your input is in the form of a string.
+    portno = atoi(argv[3]);
+
+    std::cout << "Port: " << portno << std::endl;
+    //create socket
+    //it take three arguments - address domain, type of socket, protocol (zero allows the OS to choose thye appropriate protocols based on type of socket)
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+        error("ERROR opening socket");
+
+    std::cout << "Socket opened." << std::endl;
+
+    std::cout << "Attempting to connect." << std::endl;
+    //funcion gethostbyname() takes name as an argument and returns a pointer to a hostent containing information about that host
+    //name is taken as an argument from the user
+    server = gethostbyname(argv[1]);
+
+    //If hostent structure is NULL (after the above assignment), the system could not locate a host with this name
+    if (server == NULL)
+    {
+        fprintf(stderr, "ERROR, no such host\n");
+        exit(0);
+    }
+
+    std::cout << "Connected to host: " << server->h_name << std::endl;
+
+    //set server address buffer with zeros using bzero or memset
+    //two arguments - pointer to buffer and sizeof buffer
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+
+    //following code sets the fields in serv_addr
+    //contains a code for the address family
+    serv_addr.sin_family = AF_INET;
+
+    //copies length bytes from s1 to s2
+    bcopy((char *) server->h_addr,
+          (char *) &serv_addr.sin_addr.s_addr,
+          server->h_length);
+
+    //contain the port number
+    serv_addr.sin_port = htons(portno);
+
+    //connect function is called by the client to establish a connection to the server. It takes three arguments, the socket file descriptor, the address of the host to which it wants to connect (including the port number), and the size of this address
+    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+        error("ERROR connecting");
+
+    //After a connection a client has successfully connected to the server
+    //initialize the buffer using the bzero() function
+    bzero(buffer, 256);
+
+    //both the server can read and write after a connection has been established.
+    //everything written by the client will be read by the server, and everything written by the server will be read by the client.
+
+    //READ/WRITE 1
+    //write from the argv[2] into the socket to send the client number to the serve
+    n = write(sockfd, argv[2], strlen(argv[2]));
+
+    //check of the write function was successful or not
+    if (n < 0)
+        error("ERROR writing to socket");
+
+    //Check if number of clients is maximum
+    //std::cout << "Checking if server will accept more clients..." << std::endl;
+
+    //READ/WRITE 2
+    //read response from server
+    //should be "Max active clients reached."
+    //or "There's room for you"
+    //read_server();
+
+    //READ/WRITE 3
+    //check if server closed connection
+    //should return 0
+    //Or say "Checking your client ID..."
+    //read_server();
+
+    //check if client number is valid
+    //std::cout << "Checking if client number " << argv[2] << " is valid." << std::endl;
+
+    //READ/WRITE 4
+    //read response from server
+    //Should say "Client ID already online"
+    //Or "You have been registered."
+    //read_server();
+
+    //READ/WRITE 5
+    //check if server closed connection
+    //should return 0
+    //Or say "Ready for requests."
+    read_server();
+
+    bool more_commands = false;
+    bool input_valid = false;
+
+    //while the user has commands, send requests and receive responses
+    do
+    {
+        //empty buffer
+        bzero(buffer, 256);
+
+        //prompt for input
+        std::cout << "Your message: " << std::endl;
+
+        //getinput
+        fgets(buffer,255,stdin);
+
+        //send request
+        n = write(sockfd,buffer,strlen(buffer));
+
+        //check of the write function was successful or not
+        if (n < 0)
+            error("ERROR writing to socket");
+
+        bzero(buffer, 256);
+
+        //get response
+        read_server();
+
+        do
+        {
+            std::cout << std::endl;
+            //prompt for more commands
+            std::cout << "Another command? y/n" << std::endl;
+
+            std::string input;
+
+            std::cin >> input;
+
+            if(input.compare("y") == 0)
+            {
+                std::cout << std::endl;
+                std::cout << "Continuing." << std::endl;
+                input_valid = true;
+                more_commands = true;
+            }
+            else if(input.compare("n") == 0)
+            {
+                std::cout << std::endl;
+                std::cout << "Terminating connection.  Goodbye." << std::endl;
+                input_valid = true;
+                more_commands = false;
+            }
+            else
+            {
+                std::cout << std::endl;
+                std::cout << "Invalid input.  Try again." << std::endl;
+                input_valid = false;
+            }
+        }
+        while(input_valid == false);
+
+        //clear the cin buffer
+        std::cin.clear();
+        std::cin.ignore(INT_MAX, '\n');
+    }
+    while(more_commands == true);
+
+     //close connections using file descriptors
+     close(sockfd);
+    return 0;
+}
+
+
+
+
+
---
+++ server/server.cpp
@@ -0,0 +1,441 @@
+//Name of Authros: John Hritz & Santiago Quinio
+//Course Number and Name: CSE434, Computer Networks
+//Semester: Fall 2016
+//Project Part 1
+//Time Spent: 4 hours
+
+/*
+ * 1.)    What are the advantages and disadvantages of a concurrent, connection oriented server?
+ *  A.)
+ *
+ *  Advantage:
+ *   Better performance than Iterative server implementations
+ *   Lack of wait time for clients
+ *   Overall provides a faster response time to multiple clients
+ *   Works well when a response requires massive amounts of input
+ *   Works well when processing time for each section varies
+ *   Able to handle multiple requests at the same time
+ *   Reliable delivery
+ *
+ *   Disadvantages:
+ *   More difficult to design and build
+ *   Resulting code is more complex
+ *   Code is difficult to modify
+ *
+ *   2.)    What is the appropriate usage of a concurrent, connection oriented server?
+ *   A.)
+ *   It is appropriate to use a concurrent connection oriented server when multiple clients are connecting to the server at the same time.
+ *
+ *
+ */
+
+
+/*
+ * Execution: server p
+ *
+ * p: port number for connection
+ *
+ * server -> argv[0]
+ * p -> argv[1]
+ */
+
+//input - output declarations included in all C programs
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <ios>
+#include <sstream>
+
+//contains definitions of a number of data types used in system calls
+#include <sys/types.h>
+
+//definitions of structures needed for sockets
+#include <sys/socket.h>
+
+//in.h contains constants and structures needed for internet domain addresses
+#include <netinet/in.h>
+
+//contains functions for checking the state of child processes
+#include <sys/wait.h>
+
+//vector for the user ids
+std::vector<std::string> users;
+
+//vector for the child pids
+std::vector<int> child_pids;
+
+//number of active children
+int active_children = 0;
+
+//variable declarations
+
+//sockfd and newsockfd are file descriptors,These two variables store the values returned by the socket system call and the accept system call.
+//portno stores the port number on which the server accepts connections.
+int sockfd, newsockfd, portno;
+
+
+
+//This function is called when a system call fails. It displays a message about the error on stderr and then aborts the program.
+void error(const char *msg)
+{
+    perror(msg);
+    exit(0);
+}
+
+//This function is called when a system call in a child process fails. It displays a message about the error on stderr and then aborts the program.
+void child_error(const char *msg)
+{
+    perror(msg);
+    _exit(0);
+}
+
+
+void client_service()
+{
+    int n;
+    char buffer[256];
+
+    //while connection is open
+    while(1)
+    {
+        std::cout << "Waiting for request from client " << users.back() << std::endl;
+
+        bzero(buffer,256);
+        n = read(newsockfd,buffer,255);
+
+        if (n < 0)
+        {
+            child_error("ERROR reading from socket");
+        }
+
+        if (n == 0)
+        {
+            child_error("Connection closed by client");
+        }
+
+        std::cout << "Message from client " << users.back() << ": " << buffer << std::endl;
+
+        bzero(buffer,256);
+        n = write(newsockfd,"I got your message",18);
+    }
+
+    //_exit(0);
+}
+
+void check_children()
+{
+    //holds the state returned by waitpid()
+    pid_t state;
+
+    //needed for waitpid()
+    int status;
+
+    //pid of the current child being checked
+    int current_child;
+
+    //for child in child_pids
+    for(int child = 0; child < child_pids.size(); child++)
+    {
+        current_child =  child_pids[child];
+
+        std::cout << "Checking child process " << current_child << "..." << std::endl;
+
+        //get state of process
+        state = waitpid(current_child, &status, WNOHANG);
+
+        //if state is 0, child is alive
+        if(state == 0)
+        {
+            std::cout << "Child process " << current_child << " is alive." << std::endl;
+        }
+        //else if state is -1, error has occurred
+        else if(state == -1)
+        {
+            //print error
+            std::cout << "ERROR in child process " << current_child << std::endl;
+            error("Terminating.");
+        }
+        //else child exited
+        else
+        {
+            std::cout << "Child process " << current_child << " is dead.  Removing..." << std::endl;
+            //remove child from child_pids
+            child_pids.erase(child_pids.begin() + child);
+
+            std::cout << "Child process " << current_child << " removed." << std::endl;
+
+            //decrement active_children
+            active_children--;
+
+            std::cout << current_child << " active child processes." << std::endl;
+        }
+    }
+
+
+}
+
+int main(int argc, char *argv[])
+{
+    //if too few/many arguments are provided
+    if(argc != 2)
+    {
+        error("ERROR: No port number specified.  Correct command usage is: server <port>.");
+    }
+
+    //if argv[1] is not an integer
+    if (strtol(argv[1], NULL, 0) == 0)
+    {
+        error("ERROR: Invalid port number specified.  Port must be an integer.  Correct command usage is: server <port>.");
+    }
+
+    //if argv[1] is <= 1024
+    if(atoi(argv[1]) <= 1024)
+    {
+        error("ERROR: Ports numbered 1024 and below are reserved by the OS.");
+    }
+
+    //clilen stores the size of the address of the client. This is required for the accept system call.
+    socklen_t clilen;
+
+    //server reads characters from the socket connection into this buffer.
+    std::cout << "Allocating IO buffer..." << std::endl;
+    char buffer[256];
+    std::cout << "IO buffer allocated." << std::endl;
+
+
+    //sockaddr_in is a structure containing an internet address
+    /*
+    struct sockaddr_in
+    {
+      short   sin_family; //must be AF_INET
+      u_short sin_port;
+      struct  in_addr sin_addr;
+      char    sin_zero[8]; // Not used, must be zero
+    };
+    */
+    //in_addr structure, contains only one field, a unsigned long called s_addr.
+
+    //serv_addr will contain the address of the server, and cli_addr will contain the address of the client which connects to the server.
+    struct sockaddr_in serv_addr, cli_addr;
+    int n;
+    int pid;
+
+    //create socket
+    //it take three arguments - address domain, type of socket, protocol (zero allows the OS to choose thye appropriate protocols based on type of socket)
+    std::cout << "Creating socket..." << std::endl;
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+    {
+        error("ERROR opening socket");
+    }
+    std::cout << "Socket created." << std::endl;
+
+    //set server address buffer with zeros using bzero or memset
+    //two arguments - pointer to buffer and sizeof buffer
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+
+    //atoi() function can be used to convert port number from a string of digits to an integer, if your input is in the form of a string.
+    std::cout << "Assigning port " << atoi(argv[1]) << "..." << std::endl;
+    portno = atoi(argv[1]);
+    std::cout << "Assigned port: " << portno << std::endl;
+
+    //contains a code for the address family
+    std::cout << "Assigning address family..." << std::endl;
+    serv_addr.sin_family = AF_INET;
+    std::cout << "Address family assigned." << std::endl;
+
+    //contains the IP address of the host
+    std::cout << "Storing IP address..." << std::endl;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    std::cout << "IP address stored." << std::endl;
+
+    //contain the port number
+    std::cout << "Storing port number..." << std::endl;
+    serv_addr.sin_port = htons(portno);
+    std::cout << "Port number stored." << std::endl;
+
+    //bind() system call binds a socket to an address, in this case the address of the current host and port number on which the server will run.
+    //three arguments, the socket file descriptor, the address to which is bound, and the size of the address to which it is bound.
+    std::cout << "Binding socket..." << std::endl;
+    if (bind(sockfd, (struct sockaddr *) &serv_addr,
+             sizeof(serv_addr)) < 0)
+    {
+        error("ERROR on binding");
+    }
+    std::cout << "Socket bound." << std::endl;
+
+    while(1)
+    {
+        //get state of child processes
+        std::cout << "Checking state of child processes..." << std::endl;
+        check_children();
+
+        //listen system call allows the process to listen on the socket for connections.
+        //The first argument is the socket file descriptor, and second is number of connections that can be waiting while the process is handling a particular connection.
+        std::cout << "Listening for connections..." << std::endl;
+        listen(sockfd,5);
+
+        std::cout << "Connection detected." << std::endl;
+        clilen = sizeof(cli_addr);
+
+        //accept() system call causes the process to block until a client connects to the server.
+        std::cout << "Accepting connection..." << std::endl;
+        newsockfd = accept(sockfd,
+                           (struct sockaddr *) &cli_addr,
+                           &clilen);
+
+        if (newsockfd < 0)
+        {
+            error("ERROR on accept");
+        }
+        std::cout << "Connection accepted." << std::endl;
+
+        //After a connection a client has successfully connected to the server
+        //initialize the buffer using the bzero() function
+        bzero(buffer,256);
+
+        //reads the client number from the socket into a buffer for a maximum of 255 characters
+        //read call uses new file descriptor, the one returned by accept()
+
+        std::cout << "Acquiring client number..." << std::endl;
+
+        //READ/WRITE 1
+        //read client number from client
+        n = read(newsockfd,buffer,255);
+
+        if (n < 0)
+        {
+            error("ERROR reading from socket");
+        }
+
+        //convert client number to string
+        std::string client_data(buffer);
+
+        printf("Incoming client: %s\n",buffer);
+
+
+        std::cout << "Checking client count..." << std::endl;
+
+        //Check child count
+        //if child count is >= 5, reject connection
+        if(active_children >= 5)
+        {
+            std::cout << "Maximum clients reached.  Rejecting new client." << std::endl;
+
+            /*
+            //READ/WRITE 2
+            //reject connection with close(client_socket)
+            n = write(newsockfd,"Max active clients reached.",18);
+
+            if (n < 0)
+            {
+                error("ERROR writing to socket");
+            }
+            */
+            //READ/WRITE 3
+            close(newsockfd);
+
+            std::cout << "Incoming socket closed." << std::endl;
+
+        }
+        //else notify and continue
+        else
+        {
+            std::cout << "Maximum clients not reached.  Continuing." << std::endl;
+
+            //READ/WRITE 2
+            /*
+            n = write(newsockfd,"There's room for you",18);
+
+            if (n < 0)
+            {
+                error("ERROR writing to socket");
+            }
+
+            //READ/WRITE 3
+            n = write(newsockfd,"Checking your client ID...",18);
+
+            if (n < 0)
+            {
+                error("ERROR writing to socket");
+            }
+             */
+
+            std::cout << "Checking if client " << client_data << " is registered." << std::endl;
+
+            //If client is in client_id list, reject them and close the connection
+            if(std::find(users.begin(), users.end(), client_data) != users.end())
+            {
+                std::cout << "Client " << client_data << " is already registered." << std::endl;
+
+                /*
+                //READ/WRITE 4
+                //reject connection with close(client_socket)
+                n = write(newsockfd,"Client ID already online",18);
+
+                if (n < 0)
+                {
+                    error("ERROR writing to socket");
+                }
+                 */
+
+                //READ/WRITE 5
+                close(newsockfd);
+
+                std::cout << "Incoming socket closed." << std::endl;
+            }
+            //else, add them to the list and fork
+            else
+            {
+                //add user to clients
+                users.push_back(client_data);
+                std::cout << "Client " << users.back() << " now registered." << std::endl;
+
+                /*
+                //READ/WRITE 4
+                n = write(newsockfd,"You have been registered.",18);
+
+                if (n < 0)
+                {
+                    error("ERROR writing to socket");
+                }
+                 */
+
+
+                pid = fork(); //Program spawns child process here
+                //fork() returns the process id (pid) of the child process to the parent and 0 to the child.
+
+                if (pid == 0) //if we are in the child process <-- child execution starts here
+                {
+                    //Handle requests from client
+                    std::cout << "Forked to handle client " << users.back() << "." << std::endl;
+
+                    //READ/WRITE 5
+                    n = write(newsockfd,"Ready for requests.",18);
+
+                    if (n < 0)
+                    {
+                        error("ERROR writing to socket");
+                    }
+
+                    client_service();
+                }
+                //else add pid to child_pids
+                else
+                {
+                    child_pids.push_back(pid);
+                    //increment number of child processes
+                    active_children++;
+                }
+            }
+        }
+    }
+
+    return 0;
+}
+
+
